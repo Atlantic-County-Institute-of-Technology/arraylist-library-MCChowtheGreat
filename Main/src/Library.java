9cf26090ceeedcd736ff42633a41fdfa7976064c
MCChowtheGreat <chow8801@student.acitech.org>
2025-03-07 20:34:35 -0500

    3/7/25 (Final Submit)
    -Finished the code
    -Hit the requiremnts
    -Added Comments
    - Added a test queue of songs
    -Added a loop to keep the code running till ended.

--- Main/src/Library.java
+++ Main/src/Library.java
@@ -1,24 +1,24 @@
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Libary {
-    ArrayList<String> collection = new ArrayList<>();
+public class Library {
+    ArrayList<String> collection = new ArrayList<>();  //Creates array list to hold information
 
-    public void addItem(String item) {
+    public void addItem(String item) {  //Used to add items to Library
 collection.add(item);
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty(){  //Used when the Library is empty
         return collection.isEmpty();
 
     }
-    public void removeItem(String item) {
+    public void removeItem(String item) { //Used to remove items in the library
 
         collection.remove(item);
         }
-    public String getNextItem() {
-        if (collection.isEmpty()) {
-            return "Libary is empty.";
+    public String getNextItem() {       //Used to get the next item in the library before the one being played.
+        if (collection.isEmpty()) {     //If there was nothing there the library returns empty
+            return "Library is empty.";
         }
 
         else {
@@ -26,12 +26,13 @@ collection.add(item);
         }
     }
 
-    public void skipNextItem() {
-        if (!collection.isEmpty()){
+    public void skipNextItem() {        //Used to skip the current thing playing
+        if (!collection.isEmpty()){     //If collection is not empty remove the next song
             collection.remove(0);
         }
     }
 
+    //Sorts the queue in alphabetic order using bubble sorts by creating temp and swaping the words.
     public void sortAlphabet() {
         int n = collection.size();
         for (int i = 0; i < n - 1; i++) {
@@ -45,16 +46,17 @@ collection.add(item);
         }
     }
 
+    //Shuffles the list in the library using a loop that swaps all the positions of the values in the list.
     public void shuffle() {
         Random rand = new Random();
-        for ( int i = collection.size() - 1; i > 0; i--) {
+        for (int i = collection.size() - 1; i > 0; i--) {
             int j = rand.nextInt(i + 1);
-            String temp = collection.get(j);
+            String temp = collection.get(i);
             collection.set(i, collection.get(j));
             collection.set(j, temp);
         }
         }
-
+//Used to find whatever is in the queue and moves it up to next in queue.
     public int find(String item) {
         for (int i = 0; i < collection.size(); i++) {
             if (collection.get(i).equals(item)) {
--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,35 +1,87 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    Libary libary = new Libary();
-    Scanner scanner = new Scanner(System.in);
+    Library queue = new Library();  //calls for the library class
+    Scanner scanner = new Scanner(System.in);   //Lets user input things
     String currentlyPlaying = "";
 
-    System.out.println("Choose an option: Play, Skip, Add, Remove, Sort, Shuffle, Find, Exit");
-    String choice = scanner.nextLine();
+    //Added songs to test the code below
+    queue.addItem("Hot n Cold");
+    queue.addItem("Bad Romance");
+    queue.addItem("Poker Face");
+    queue.addItem("Last Friday Night");
+    queue.addItem("S&M");
 
-    if (choice.equals("Play")) {
-        if (!libary.isEmpty()) {
-            currentlyPlaying = libary.getNextItem();
-            libary.skipNextItem();
-            System.out.println("Now Playing: " + currentlyPlaying);
+    //loops the code until otherwise
+        boolean loop = true;
+    while (loop) {
+
+        System.out.println("Choose an option: Play, Skip, Add, Remove, Sort, Shuffle, Find, Exit"); //Tells the user what they can pick from
+        String choice = scanner.nextLine(); //User can type, case-sensitive
+
+        //If user types in "Play" and the queue is not empty the current song in the list will play
+        if (choice.equals("Play")) {
+            if (!queue.isEmpty()) {
+                currentlyPlaying = queue.getNextItem();
+                queue.skipNextItem();
+                System.out.println("Now Playing: " + currentlyPlaying);
+            } else {
+                System.out.println("Queue is empty"); //If queue is empty this line will print
+            }
         }
+
+        //If user types in "Skip" the song currently playing will skip on to the next
+        else if (choice.equals("Skip")) {
+            queue.skipNextItem();
+            System.out.println("Skipped the next item.");
+        }
+        //If user types in "Add" then another prompts comes up to add another value which will then be added to the queue
+        else if (choice.equals("Add")) {
+            System.out.println("Enter item to add: ");
+            String addItem = scanner.nextLine();
+            queue.addItem(addItem);
+            System.out.println(addItem + " has been added to the queue.");
+        }
+        //If user types in "Remove" then another prompts comes up to remove a value from the list.
+        else if (choice.equals("Remove")) {
+            System.out.println("Enter item to remove: ");
+            String removeItem = scanner.nextLine();
+            queue.removeItem(removeItem);
+            System.out.println(removeItem + " has been removed to the queue.");
+        }
+        //If user types in "Sort" the queue will be sorted in alphabetic order.
+        else if (choice.equals("Sort")) {
+            queue.sortAlphabet();
+            System.out.println("Queue sorted");
+        }
+        //If user types in "Shuffle" the queue gets shuffled
+        else if (choice.equals("Shuffle")) {
+            queue.shuffle();
+            System.out.println("Queue has been shuffled");
+        }
+        //If user types in "Find" another prompts comes up for the user to type in the song, if the song is found the song is moved up to the next in queue.
+        else if (choice.equals("Find")) {
+            System.out.println("Enter item to find: ");
+            String findItem = scanner.nextLine();
+            int position = queue.find(findItem);
+            if (position >= 0) {
+                System.out.println(findItem + " found at position: " + position);
+            }
+            else {
+                System.out.println(findItem + " can not be found in the queue");
+            }
+        }
+        //If user types in "Exit" the code ends
+        else if (choice.equals("Exit")){
+            System.out.println("Closing program");
+            scanner.close();
+            break;
+        }
+        //If the user types anything not within the code there will be a prompt to try again.
         else {
-            System.out.println("Libary is empty");
+            System.out.println("Please try again");
         }
     }
-    else if (choice.equals("Skip")){
-        libary.skipNextItem();
-        System.out.println("Skipped the next item.");
-        }
-    else if (choice.equals("Add")){
-        System.out.println("Enter item to add: ");
-        String addItem = scanner.nextLine();
-        libary.addItem(addItem);
-        System.out.println(addItem + " added to the libary.");
-    }
-
     }
 }
